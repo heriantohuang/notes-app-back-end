37a23781f25d4b3434825aacf5303d94756fd393
herianto <herianto.bti@panin.co.id>
2023-02-24 14:21:55 +0700

    collaboration implemented

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -6,8 +6,9 @@ const NotFoundError = require('../../exceptions/NotFoundError');
 const AuthorizationError = require('../../exceptions/AuthorizationError');
 
 class NotesServices {
-  constructor() {
+  constructor(collaborationService) {
     this._pool = new Pool();
+    this._collaborationService = collaborationService;
   }
 
   async addNote({
@@ -33,7 +34,10 @@ class NotesServices {
 
   async getNotes(owner) {
     const query = {
-      text: 'SELECT * from notes where owner = $1',
+      text: `SELECT notes.* FROM notes 
+      LEFT JOIN collaborations on collaborations.note_id = notes.id
+      WHERE notes.owner = $1 OR collaborations.user_id = $1
+      GROUP BY notes.id`,
       values: [owner],
     };
 
@@ -43,7 +47,10 @@ class NotesServices {
 
   async getNoteById(id) {
     const query = {
-      text: 'SELECT * FROM notes where id = $1',
+      text: `SELECT notes.*, users.username 
+      FROM notes 
+      LEFT JOIN users on users.id = notes.owner
+      where notes.id = $1`,
       values: [id],
     };
     const result = await this._pool.query(query);
@@ -98,6 +105,22 @@ class NotesServices {
 
     if (note.owner !== owner) {
       throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async verifyNoteAccess(noteId, userId) {
+    try {
+      await this.verifyNoteOwner(noteId, userId);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error;
+      }
+
+      try {
+        await this._collaborationService.verifyCollaborator(noteId, userId);
+      } catch {
+        throw error;
+      }
     }
   }
 }
